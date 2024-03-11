cb3befaf528605e6693a0952aed3985a5cba438b
Didier Restrepo <direstrepobr@icloud.com>
2024-03-11 10:48:41 -0500

    stock available online

--- custom-addons/pos_stock_available_online/__manifest__.py
+++ custom-addons/pos_stock_available_online/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "Point of Sale Stock Available Online",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.3",
     "category": "Sales/Point of Sale",
     "summary": "Show the available quantity of products in the Point of Sale ",
     "depends": ["point_of_sale", "stock_available", "base_automation"],
     "website": "https://github.com/OCA/pos",
     "author": "Cetmix, Odoo Community Association (OCA)",
-    "maintainers": ["GabbasovDinar", "CetmixGitDrone"],
+    "images": ["static/description/banner.png"],
     "installable": True,
     "data": ["views/res_config_settings_view.xml"],
     "assets": {
